b0a5b6c074275d1d4380ab832f5604c3594438f7
Leslie Dominguez <LeslieDominguez601@Gmail.com>
2018-12-20 13:12:47 -0500

    new

---
+++ pocket-calculator/java/pCalculator.js
@@ -0,0 +1,67 @@
+function clear(){
+let p = document.getElementById('display').innerHTML = " ";
+}
+
+function negation(){
+}
+
+function percentage(){
+}
+
+function division(){
+}
+
+function seven(){
+  let p = document.getElementById('display').innerHTML = "7";
+}
+
+function eight(){
+    let p = document.getElementById('display').innerHTML = "8";
+}
+
+function nine(){
+    let p = document.getElementById('display').innerHTML = "9";
+}
+
+function multiplication(){
+}
+
+function four(){
+  let p = document.getElementById('display').innerHTML = "4";
+}
+
+function five(){
+    let p = document.getElementById('display').innerHTML = "5";
+}
+
+function six(){
+    let p = document.getElementById('display').innerHTML = "6";
+}
+
+function subtraction(){
+}
+
+function one(){
+    let p = document.getElementById('display').innerHTML = "1";
+}
+
+function two(){
+    let p = document.getElementById('display').innerHTML = "2";
+}
+
+function three(){
+    let p = document.getElementById('display').innerHTML = "3";
+}
+
+function addition(){
+}
+
+function zero(){
+    let p = document.getElementById('display').innerHTML = "0";
+}
+
+function decimal(){
+}
+
+function equals(){
+}
